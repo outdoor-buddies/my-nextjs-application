c36487faec23a26db19402120732a31f27f6661a
KellyM12 <150403430+KellyM12@users.noreply.github.com>
2026-08-11 21:43:27 -1000

    Remove dynamic export from Navbar
    Removed dynamic export from Navbar component.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,8 +11,6 @@ import { Container, Nav, Navbar, NavDropdown, Image } from 'react-bootstrap';
 import { BoxArrowRight, PersonFill, PersonPlusFill } from 'react-bootstrap-icons';
 import Link from 'next/link';
 
-export const dynamic = 'force-dynamic';
-
 const NavBar: React.FC = () => {
   const { data: session, status } = useSession();
   const pathName = usePathname();
